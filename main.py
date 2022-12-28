476508a275894de66ab21adb83788207bdf763ae
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-28 13:15:14 +0300

    Warming up to event listeners

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-""""
+"""
     The goal here is to demonstrate and practice the use of event listeners.
 
     Will be creating multiple games in which I will apply the concept.
 
-    Key Take aways.
+    Key Takeaways.
         1. When using a function as an argument you do not add parentheses.
-        2. It is recommended to use keyword arguments eg my_function(c = 3, a = 1, b = 2)
-           instead of positional arguments eg my_function(1, 2, 3) when working with functions
+        2. It is recommended to use keyword arguments e.g. my_function(c = 3, a = 1, b = 2)
+           instead of positional arguments e.g. my_function(1, 2, 3) when working with functions
            you did not create.
 """
 
